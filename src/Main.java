c78a8518a1efdaa2f850774f3bee62147f4f91ed
YuriyM <Mikchailov1988@mail.ru>
2025-07-24 21:16:55 +0300

    Task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Привет,мир!");
+public class Main{
+    public static void main(String[] args){
+        var dog = 8.0;
+        System.out.println("Собака " +dog);
+        var cat = 3.6;
+        System.out.println("Кошка " +cat);
+        var paper = 763789;
+        System.out.println("Бумага " +paper);
     }
 }
