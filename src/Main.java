78ea267e0e69031d3c687e5906d3d8a13ac8a66e
YuriyM <Mikchailov1988@mail.ru>
2025-07-25 12:02:25 +0300

    Task3

--- src/Main.java
+++ src/Main.java
@@ -9,15 +9,26 @@ public class Main{
         System.out.println("Бумага " +paper);
 
         System.out.println("Задание 2");
-        var dog1 = 8.0;
-        dog1 = dog1 + 4;
-        System.out.println("Собака плюс 4 = " +dog1);
-        var cat1 = 3.6;
-        cat1 = cat1 + 4;
-        System.out.println("Кошка плюс 4 " +cat1);
-        var paper1 = 763789;
-        paper1 = paper1 + 4;
-        System.out.println("Бумага плюс 4 = " + paper1);
+        var dog2 = 8.0;
+        dog2 = dog2 + 4;
+        System.out.println("Собака плюс 4 = " +dog2);
+        var cat2 = 3.6;
+        cat2 = cat2 + 4;
+        System.out.println("Кошка плюс 4 " +cat2);
+        var paper2 = 763789;
+        paper2 = paper2 + 4;
+        System.out.println("Бумага плюс 4 = " + paper2);
+
+        System.out.println("Задание 3");
+        var dog3 = 8.0;
+        dog3 = dog3 - 3.5;
+        System.out.println("Собака минус 3.5 = " +dog3);
+        var cat3 = 3.6;
+        cat3 = cat3 - 1.6;
+        System.out.println("Кошка минус 1.6 =  " +cat3);
+        var paper3 = 763789;
+        paper3 = paper3 - 7639;
+        System.out.println("Бумага минус 7639 =  " +paper3);
 
 
     }
