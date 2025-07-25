152289b5c2fdd4bc62305d566458630d1ec84f44
YuriyM <Mikchailov1988@mail.ru>
2025-07-25 11:43:50 +0300

    Task2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,24 @@
 public class Main{
     public static void main(String[] args){
+        System.out.println("Задание 1");
         var dog = 8.0;
         System.out.println("Собака " +dog);
         var cat = 3.6;
         System.out.println("Кошка " +cat);
         var paper = 763789;
         System.out.println("Бумага " +paper);
+
+        System.out.println("Задание 2");
+        var dog1 = 8.0;
+        dog1 = dog1 + 4;
+        System.out.println("Собака плюс 4 = " +dog1);
+        var cat1 = 3.6;
+        cat1 = cat1 + 4;
+        System.out.println("Кошка плюс 4 " +cat1);
+        var paper1 = 763789;
+        paper1 = paper1 + 4;
+        System.out.println("Бумага плюс 4 = " + paper1);
+
+
     }
 }
